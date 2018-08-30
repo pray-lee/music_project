7a54a8e7677f7fe01205c706dc3a67cbe4129340
pray-lee <praycis@gmail.com>
2018-08-30 18:10:38 +0800

    add views

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
     <router-view/>
   </div>
 </template>
 
 <script>
 import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/Tab'
 export default {
   name: 'App',
   components: {
-    'm-header': MHeader
+    'm-header': MHeader,
+    Tab
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,33 @@
-import Vue from 'vue'
+import vue from 'vue'
 import Router from 'vue-router'
+import Recommend from 'components/recommend/Recommend'
+import Rank from 'components/rank/Rank'
+import Singer from 'components/singer/Singer'
+import Search from 'components/search/Search'
 
-Vue.use(Router)
+vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect: '/recommend'
+    },
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/singer',
+      component: Singer
+    },
+    {
+      path: '/rank',
+      component: Rank
+    }
   ]
 })
